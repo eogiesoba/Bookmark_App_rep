f432ef028df94a70b2a9164ea12fca2972d694d6
eogiesoba <eogiesoba@gmail.com>
2018-03-08 17:29:37 -0600

    Updated the api routes

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
