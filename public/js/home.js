bfc4fb062a6d2f158b33b5253413adf0ef64f60d
eogiesoba <eogiesoba@gmail.com>
2018-03-15 14:54:40 -0500

    Updated the image colors

--- public/js/home.js
+++ public/js/home.js
@@ -265,7 +265,7 @@ $(document).ready(function () {
             garbageDiv.addClass("row")
             garbageDiv.addClass("deleteStyle");
         // garbageDiv.append("<div class='col-md-3'><button class='garbageBtn' id='trash' gid='" + bookmarkData[j].id + "'><i class='fas fa-trash-alt'></i></button></div>");
-            garbageDiv.append("<img src='../images/Garbage.png' />");
+            garbageDiv.append("<img src='../images/Garbage.png' class='imageClass' />");
             garbageDiv.attr("gid", bookmarkData[j].id);
             garbageDiv.attr("id", "trash");
             bigBMDiv.append(garbageDiv);
@@ -314,6 +314,7 @@ $(document).ready(function () {
         var folderLine = $("<div>");
         folderLine.addClass("row");
         folderLine.addClass("folderList");
+        folderLine.append("<img src='../images/Search.png' class='imageClass' />");
         // folderLine.attr("foldername", folderData.folder);
         folderLine.attr("folderId", folderData.id);
         folderLine.attr("userNo", folderData.UserId);
