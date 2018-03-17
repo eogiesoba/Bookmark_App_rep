1c67fc3058b57c9b04c31f583a16394a85e6a48b
eogiesoba <eogiesoba@gmail.com>
2018-03-17 10:43:56 -0500

    Created new folder delete buttons

--- public/js/home.js
+++ public/js/home.js
@@ -319,6 +319,12 @@ $(document).ready(function () {
         folderLabelDiv.attr("draggable", true);
         folderLabelDiv.attr("ondragstart", "drag(event)");
         folderLabelDiv.append("<p class='folderLabelDivText' folderId='" + folderData.id + "' >" + folderData.folder + "</p>");
+
+        var deleteFolderBtn = $("<button>");
+        deleteFolderBtn.addClass("deleteFolderBtn");
+        deleteFolderBtn.attr("id", folderData.id);
+
+        folderLine.append(deleteFolderBtn);
         folderLine.append(folderLabelDiv);
 
 
