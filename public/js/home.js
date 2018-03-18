1d3dbcdfb7d945f452ccfa3c7887854fd7c24d3f
Faizy711 <faizan.s711@gmail.com>
2018-03-18 18:50:48 -0500

    login bar

--- public/js/home.js
+++ public/js/home.js
@@ -223,7 +223,7 @@ $(document).ready(function () {
         var tableFID;
         var BFN;
         $('.bmBox').remove();
-        bar.animate(1.0);
+        // bar.animate(1.0);
 
         console.log("bookData", bookmarkData);
         for (var j = 0; j < bookmarkData.length; j++) {
@@ -392,34 +392,34 @@ $(document).ready(function () {
     // progressbar.js@1.0.0 version is used
     // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
-    var bar = new ProgressBar.Line(container, {
-        strokeWidth: 4,
-        easing: 'easeInOut',
-        duration: 1400,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: { width: '100%', height: '100%' },
-        text: {
-            style: {
-                // Text color.
-                // Default: same as stroke color (options.color)
-                color: '#999',
-                position: 'absolute',
-                right: '0',
-                top: '30px',
-                padding: 0,
-                margin: 0,
-                transform: null
-            },
-            autoStyleContainer: false
-        },
-        from: { color: '#FFEA82' },
-        to: { color: '#ED6A5A' },
-        step: (state, bar) => {
-            bar.setText(Math.round(bar.value() * 100) + ' %');
-        }
-    });
+    // var bar = new ProgressBar.Line(container, {
+    //     strokeWidth: 4,
+    //     easing: 'easeInOut',
+    //     duration: 1400,
+    //     color: '#FFEA82',
+    //     trailColor: '#eee',
+    //     trailWidth: 1,
+    //     svgStyle: { width: '100%', height: '100%' },
+    //     text: {
+    //         style: {
+    //             // Text color.
+    //             // Default: same as stroke color (options.color)
+    //             color: '#999',
+    //             position: 'absolute',
+    //             right: '0',
+    //             top: '30px',
+    //             padding: 0,
+    //             margin: 0,
+    //             transform: null
+    //         },
+    //         autoStyleContainer: false
+    //     },
+    //     from: { color: '#FFEA82' },
+    //     to: { color: '#ED6A5A' },
+    //     step: (state, bar) => {
+    //         bar.setText(Math.round(bar.value() * 100) + ' %');
+    //     }
+    // });
 
     
 
