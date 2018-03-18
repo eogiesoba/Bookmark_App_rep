5be729dd34df6a906d08809bdb09de38f211eb05
Faizy711 <faizan.s711@gmail.com>
2018-03-18 18:29:31 -0500

    login bar

--- public/js/home.js
+++ public/js/home.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
     //validateUser();//This our bookmark render function that runs when page is loaded. 
     //renderFolders();//This is our folder render function that runs when page is loaded.
 
-    // document.getElementById("userName").value = localStorage.getItem("BookmarkUserEmail");
-    document.getElementById("modaluserName").value = localStorage.getItem("BookmarkUserEmail");
+
+
 
     //Global variables
     // var userID;//This is being used in a get request
@@ -388,7 +388,39 @@ $(document).ready(function () {
         $(".folderList").remove();
     }
 
+    // progressbar.js@1.0.0 version is used
+    // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
+    var bar = new ProgressBar.Line(container, {
+        strokeWidth: 4,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#FFEA82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: { width: '100%', height: '100%' },
+        text: {
+            style: {
+                // Text color.
+                // Default: same as stroke color (options.color)
+                color: '#999',
+                position: 'absolute',
+                right: '0',
+                top: '30px',
+                padding: 0,
+                margin: 0,
+                transform: null
+            },
+            autoStyleContainer: false
+        },
+        from: { color: '#FFEA82' },
+        to: { color: '#ED6A5A' },
+        step: (state, bar) => {
+            bar.setText(Math.round(bar.value() * 100) + ' %');
+        }
+    });
+
+    bar.animate(1.0);
 
 });
 
