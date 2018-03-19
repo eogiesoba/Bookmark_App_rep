e77a170bad55e0cff06f6e19fb2539da963789c8
eogiesoba <eogiesoba@gmail.com>
2018-03-18 19:44:09 -0500

    Commented the html routes to test usage

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,20 +1,20 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
+// // *********************************************************************************
+// // html-routes.js - this file offers a set of routes for sending users to the various html pages
+// // *********************************************************************************
 
-// Dependencies
-// =============================================================
-var path = require("path");
+// // Dependencies
+// // =============================================================
+// var path = require("path");
 
-// Routes
-// =============================================================
-module.exports = function(app) {
+// // Routes
+// // =============================================================
+// module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+//   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+//   // index route loads view.html
+//   app.get("/home", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+//   });
 
-};
+// };
